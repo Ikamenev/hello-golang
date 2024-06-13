31aaec3b98e814e93cc779614122b12a6989645d
Ivan Kamenev <vk-v8@mail.ru>
2024-06-13 12:49:10 +1000

    fix error

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ikamenev/hello-golang
+
+go 1.22.0
--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHello(t *testing.T) {
 	want := "Hello Go"
 
-	got := "Hello Go"
+	got := hello()
 
 	if want != got {
 		t.Fatalf("want %s, got %s\n", want, got)
